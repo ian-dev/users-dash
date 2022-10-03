ae989ca138b3fde083d0b9e6eb82a787198e03b3
Julian <jleon@livevox.com>
2022-10-03 17:02:40 -0500

    test(users table) Table.js component test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { Provider as ReduxProvider } from "react-redux";
+import { store as reduxStore } from "./utils/store";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  render(
+    <ReduxProvider store={reduxStore}>
+      <App />
+    </ReduxProvider>
+  );
 });
--- src/components/Table.js
+++ src/components/Table.js
@@ -14,7 +14,7 @@ import Button from "@mui/material/Button";
 const USERS = [];
 
 export default function UsersTable() {
-  const { data = USERS, isLoading, refetch } = useGetUsersQuery();
+  const { data = USERS } = useGetUsersQuery();
   const [open, setOpen] = useState(false);
   const [pageSize, setPageSize] = useState(10);
   const [selectedUser, setSelectedUser] = useState();
@@ -105,7 +105,7 @@ export default function UsersTable() {
   );
 
   return (
-    <>
+    <div data-testid="users-table">
       <DataGrid
         rows={updatedUsers}
         columns={memoColumns}
@@ -122,6 +122,6 @@ export default function UsersTable() {
         callback={closeAlert}
         userToDelete={deleteSelectedUser}
       />
-    </>
+    </div>
   );
 }
